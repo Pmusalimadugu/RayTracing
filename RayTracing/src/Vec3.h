ece602b8ef524428a28296746c5564bfbad7b5d6
Pranay M <pmusalimadugu@gmail.com>
2021-12-20 04:31:46 -0500

    remove to string

--- RayTracing/src/Vec3.h
+++ RayTracing/src/Vec3.h
@@ -57,9 +57,6 @@ public:
 	float e[3];
 };
 
-inline std::ostream& operator<<(std::ostream& out, const Vec3& v) {
-	return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
-}
 
 inline Vec3 operator+(const Vec3& u, const Vec3& v) {
 	return Vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
