8e4021c2a2c5ffdc89d13377b4b38ad3ff6c6ad1
Pranay M <pmusalimadugu@gmail.com>
2021-12-20 02:30:28 -0500

    complete Ray.h

--- RayTracing/src/Ray.h
+++ RayTracing/src/Ray.h
@@ -5,8 +5,12 @@ class Ray {
 public:
 	Ray()
 		: origin(Vec3(0.0f, 0.0f, 0.0f)), direction(Vec3(0.0f, 0.0f, 0.0f)) {}
-	Ray(const Vec3& origin, const Vec3& direction)
+	Ray(const Point3& origin, const Vec3& direction)
 		: origin(origin), direction(direction) {}
+
+	Point3 getOrigin()  const { return origin; }
+	Vec3 getDirection() const { return direction; }
+
 	Point3 at(float t) {
 		return origin + direction * t;
 	}
